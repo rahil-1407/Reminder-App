95e11b7703ae2e9440246ed10258aeaf4414ac06
Rahil Sinha <rsinha@deqode.com>
2021-08-03 20:25:05 +0530

    added redux state to local storage

--- src/store.js
+++ src/store.js
@@ -1,9 +1,17 @@
 import rootReducer from './reducers/index'
 import { createStore } from 'redux'
+import { loadState, saveState } from './localStorage'
+
+const persistedState = loadState()
 
 const store = createStore(
   rootReducer,
+  persistedState,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 )
+
+store.subscribe(() => {
+  saveState({ reducer: store.getState().reducer })
+})
 
 export default store
